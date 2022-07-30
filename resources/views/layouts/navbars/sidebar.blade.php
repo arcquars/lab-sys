fc87bba6d4c7b6d6c48afcdebf750aad53e70cdf
Angel Pedro <arc.quars@gmail.com>
2022-07-29 23:29:16 -0400

    Modificar menu

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -160,6 +160,12 @@ Tip 2: you can also add an image using data-image tag
                         <p>Reporte Desechar</p>
                     </a>
                 </li>
+                <li class="nav-item @if($activePage == 'admin_reporte_admin_sinterminar') active @endif">
+                    <a class="nav-link" href="{{route('reporte.reporte_admin_sinterminar')}}">
+                        <i class="fas fa-receipt"></i>
+                        <p>Reporte sin terminar</p>
+                    </a>
+                </li>
 {{--                <li class="nav-item @if($activePage == 'admin_reporte_admin_sinterminar') active @endif">--}}
 {{--                    <a class="nav-link" href="{{route('reporte.reporte_admin_sinterminar')}}">--}}
 {{--                        <i class="fas fa-receipt"></i>--}}
