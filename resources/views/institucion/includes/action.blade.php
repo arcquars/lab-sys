2fe612221b2e289239551f7e76710a09d729e215
arcquars <arc.quars@gmail.com>
2026-01-27 04:08:58 +0000

    implementar borrar instituciones

--- resources/views/institucion/includes/action.blade.php
+++ resources/views/institucion/includes/action.blade.php
@@ -1,3 +1,6 @@
 <a href="#" onclick="editInstitucionAjax({{$id}}); return false;" class="btn btn-link float-right" title="Editar Institucion">
-    <i class="far fa-edit fa-lg"></i>
+    <i class="far fa-edit fa-lg"></i> Editar
+</a>
+<a href="#" onclick="openDeleteInstitucionAjax({{$id}}); return false;" class="btn btn-link text-danger float-right" title="Eliminar Institucion">
+    <i class="fas fa-trash fa-lg"></i> Eliminar
 </a>
